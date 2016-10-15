9d2d9a3ecb981d3a309c24de2297f6639a70c864
das <das@dasdemac-mini.local>
2016-10-15 14:15:28 +0800

    Initial Commit

---
+++ JZQTableDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JZQTableDemo
+//
+//  Created by das on 16/10/15.
+//  Copyright © 2016年 das. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JZQTableDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JZQTableDemo
+//
+//  Created by das on 16/10/15.
+//  Copyright © 2016年 das. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
